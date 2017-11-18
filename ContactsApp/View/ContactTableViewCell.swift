8cc912b4771e4d1f552b6b5947811eb64aa2074f
Vadym Shoshin <shoshin.vadik@gmail.com>
2017-11-18 11:06:54 +0200

    Contact image view adjustments

--- ContactsApp/View/ContactTableViewCell.swift
+++ ContactsApp/View/ContactTableViewCell.swift
@@ -15,6 +15,7 @@ class ContactTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         selectionStyle = .none
+        ibImageView.layer.cornerRadius = ibImageView.frame.width / 2.0
     }
 
     func update(_ contact: Contact) {
